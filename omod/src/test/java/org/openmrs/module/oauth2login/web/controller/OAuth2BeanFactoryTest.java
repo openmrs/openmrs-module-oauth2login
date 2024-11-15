2a6a95a9d08387bf36d811b021f8ebb46b71be90
Kipchumba Bett <kipchubett@gmail.com>
2024-11-15 14:52:36 +0300

    More tests + realistic env vars

--- omod/src/test/java/org/openmrs/module/oauth2login/web/controller/OAuth2BeanFactoryTest.java
+++ omod/src/test/java/org/openmrs/module/oauth2login/web/controller/OAuth2BeanFactoryTest.java
@@ -18,22 +18,46 @@ public class OAuth2BeanFactoryTest {
 	
 	@Test
 	public void resolveEnvVariables_shouldReturnResolvedValue() {
-		String value = "Database URL: ${DB_URL}";
-		System.setProperty("DB_URL", "jdbc:mysql://localhost:3306/openmrs");
+		String value = "Authorization URL: ${OAUTH_URL}";
+		System.setProperty("OAUTH_URL", "https://localhost:8080/auth");
 		
 		String resolvedValue = OAuth2BeanFactory.resolveEnvVariables(value);
 		
 		assertNotNull(resolvedValue);
-		assertEquals("Database URL: jdbc:mysql://localhost:3306/openmrs", resolvedValue);
+		assertEquals("Authorization URL: https://localhost:8080/auth", resolvedValue);
+	}
+	
+	@Test
+	public void resolveEnvVariables_shouldReturnResolvedValueWithMultipleEnvVariables() {
+		String value = "Authorization URL: ${OAUTH_URL}, Client Secret: ${CLIENT_SECRET}";
+		System.setProperty("OAUTH_URL", "https://localhost:8080/auth");
+		System.setProperty("CLIENT_SECRET", "secret");
+		
+		String resolvedValue = OAuth2BeanFactory.resolveEnvVariables(value);
+		
+		assertNotNull(resolvedValue);
+		assertEquals("Authorization URL: https://localhost:8080/auth, Client Secret: secret", resolvedValue);
+	}
+	
+	@Test
+	public void resolveEnvVariables_shouldReturnResolvedValueWithMultipleOccurrencesOfEnvVariable() {
+		String value = "Authorization URL: ${OAUTH_URL}, Authorization URL: ${OAUTH_URL}";
+		System.setProperty("OAUTH_URL", "https://localhost:8080/auth");
+		
+		String resolvedValue = OAuth2BeanFactory.resolveEnvVariables(value);
+		
+		assertNotNull(resolvedValue);
+		assertEquals("Authorization URL: https://localhost:8080/auth, Authorization URL: https://localhost:8080/auth",
+		    resolvedValue);
 	}
 	
 	@Test
 	public void resolveEnvVariables_shouldReturnOriginalValueIfNoEnvVariable() {
-		String value = "Database URL: ${DB_URL}";
+		String value = "Authorization URL: ${OAUTH_URL}";
 		
 		String resolvedValue = OAuth2BeanFactory.resolveEnvVariables(value);
 		
 		assertNotNull(resolvedValue);
-		assertEquals("Database URL: ${DB_URL}", resolvedValue);
+		assertEquals("Authorization URL: ${OAUTH_URL}", resolvedValue);
 	}
 }
