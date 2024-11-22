62ade3cf1898aab6e716564c037a2a7c6dcfb8b1
Kipchumba Bett <kipchubett@gmail.com>
2024-11-22 10:01:11 +0300

    OA-39: Resolve environment variables in OAuth2 properties (#23)

---
+++ omod/src/test/java/org/openmrs/module/oauth2login/web/controller/OAuth2BeanFactoryTest.java
@@ -0,0 +1,63 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.oauth2login.web.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class OAuth2BeanFactoryTest {
+	
+	@Test
+	public void resolveEnvVariables_shouldReturnResolvedValue() {
+		String value = "Authorization URL: ${OAUTH_URL}";
+		System.setProperty("OAUTH_URL", "https://localhost:8080/auth");
+		
+		String resolvedValue = OAuth2BeanFactory.resolveEnvVariables(value);
+		
+		assertNotNull(resolvedValue);
+		assertEquals("Authorization URL: https://localhost:8080/auth", resolvedValue);
+	}
+	
+	@Test
+	public void resolveEnvVariables_shouldReturnResolvedValueWithMultipleEnvVariables() {
+		String value = "Authorization URL: ${OAUTH_URL}, Client Secret: ${CLIENT_SECRET}";
+		System.setProperty("OAUTH_URL", "https://localhost:8080/auth");
+		System.setProperty("CLIENT_SECRET", "secret");
+		
+		String resolvedValue = OAuth2BeanFactory.resolveEnvVariables(value);
+		
+		assertNotNull(resolvedValue);
+		assertEquals("Authorization URL: https://localhost:8080/auth, Client Secret: secret", resolvedValue);
+	}
+	
+	@Test
+	public void resolveEnvVariables_shouldReturnResolvedValueWithMultipleOccurrencesOfEnvVariable() {
+		String value = "Authorization URL: ${OAUTH_URL}, Authorization URL: ${OAUTH_URL}";
+		System.setProperty("OAUTH_URL", "https://localhost:8080/auth");
+		
+		String resolvedValue = OAuth2BeanFactory.resolveEnvVariables(value);
+		
+		assertNotNull(resolvedValue);
+		assertEquals("Authorization URL: https://localhost:8080/auth, Authorization URL: https://localhost:8080/auth",
+		    resolvedValue);
+	}
+	
+	@Test
+	public void resolveEnvVariables_shouldReturnOriginalValueIfNoEnvVariable() {
+		String value = "Authorization URL: ${OAUTH_URL}";
+		
+		String resolvedValue = OAuth2BeanFactory.resolveEnvVariables(value);
+		
+		assertNotNull(resolvedValue);
+		assertEquals("Authorization URL: ${OAUTH_URL}", resolvedValue);
+	}
+}
